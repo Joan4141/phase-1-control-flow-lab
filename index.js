7dbe5132d1ae883ac433dea996e4ecf8ed46853e
Joan <joan.wafula@student.moringaschool.com>
2022-08-22 09:48:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let result
+  if (someValue <=400) {
+    result = 'This one is on me!';
+    return result
+  }
+  else if (someValue > 2000 && someValue < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+    return result
+  }
+  else if (someValue >2500) {
+    return 'No can do.';
+  }  
+}
+function ternaryCheckCity(city){
+return city === 'NYC'?  "Ok, sounds good." : "No go."
+}
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+  case 'generous':
+  return 'Thank you so much.'
+  case 'not as generous':
+   return 'Thank you.'
+  default:
+    return 'Bye.'
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+ 
+
+  
